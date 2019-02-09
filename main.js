752ba8afd7e9b23d8ad9850627e110a22cda7195
goercd <46823854+goercd@users.noreply.github.com>
2019-02-09 19:50:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,68 +1,20 @@
-﻿(function(){
+function throughOnce(fn) {
 
- 
-    function recursion() {        
-        const child = document.getElementById('ChildBlock');  
-        const hypnoticToad = document.querySelector('.hypnoticToad');
-          
- 
+}
 
-		
-        function catchParent(el) {   
-            drawGreen(el);             
-            const parent = el.parentNode;
-            const id = parent.getAttribute('id')                 
-            if(!id) {
-                setTimeout(() => {
-                    drawGreen(parent);
-                    catchParent(parent)
-                }, 300);        
-            } else {
-                decorateChildren(parent);
-                drawGreen(parent)                      
-            };
-        }
+var logThroughOnce = throughOnce(function("Hello world") {
+	
+	 var currentCount = 1;
+	 return currentCount++;
+	
+	var executed = false;
+	return function(fn); {
+if (currentCount = [i] % 2 == 0) 
+    executed = true;
 
-
-		
-        function decorateChildren(el) {         
-            const child = filterChild(el)[0];                   
-            
-            if(count === attempt) {
-                hypnoticToad.appendChild(img);
-                return;
-            } 
-
-            if(child) {
-                setTimeout(() => {
-                    drawBlack(child);                
-                    decorateChildren(child);
-                }, 300)            
-            } else {            
-                count++;        
-                setTimeout(() => {
-                    drawBlack(el);
-                    catchParent(el);
-                }, 300)                        
-            }
-        }
-        
-   
-    
-	console.log(document.querySelector('deepChild')); 
 	
 	
-		console.log(document.querySelector('sibling first')); 
-		for(let i = 0; i < 100; i++) {
-            div.style.margins = 'red';
-          }
-		
-		console.log(document.querySelector('sibling second')); 
-		for(let i = 0; i < 100; i++) {
-             div.style.margins = 'green';            
-		}
-		
-		console.log(document.querySelector('deepChild'));
-	
-	
-    }
+  console.log("Hello world");
+});
+
+
